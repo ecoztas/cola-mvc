d715b66ef70365a7ceaec75e50463b01354d8226
Emre Can ÖZTAŞ <emrecanoztas@outlook.com>
2017-12-15 21:40:04 +0200

    Version: 0.0.1 Alfa-1

--- core/system/Model.php
+++ core/system/Model.php
@@ -2,21 +2,9 @@
 
 class Model
 {
-
-    public $model;
-
     public function __construct()
     {
         
-    }
-
-    public static function getInstance()
-    {
-        if (!$this->controller instanceof self) {
-            $this->controller = new self;
-        }
-
-        return($this->controller);
     }
 
     public function loadLibraries($libraryNames)
